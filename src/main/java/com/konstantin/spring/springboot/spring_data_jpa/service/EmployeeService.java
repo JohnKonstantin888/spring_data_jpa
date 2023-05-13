5d82a83c22c397c66f2c388fa12ea92554aac5dd
Konstantin Kovalyov <Konstantin-kovalyo@yandex.ru>
2023-05-13 14:02:53 +0300

    correct service

--- src/main/java/com/konstantin/spring/springboot/spring_data_jpa/service/EmployeeService.java
+++ src/main/java/com/konstantin/spring/springboot/spring_data_jpa/service/EmployeeService.java
@@ -1,18 +1,67 @@
 package com.konstantin.spring.springboot.spring_data_jpa.service;
 
+import com.konstantin.spring.springboot.spring_data_jpa.dao.EmployeeRepository;
+import com.konstantin.spring.springboot.spring_data_jpa.dto.EmployeeDTO;
 import com.konstantin.spring.springboot.spring_data_jpa.entity.Employee;
+import com.konstantin.spring.springboot.spring_data_jpa.exceptions.EmployeeNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface EmployeeService {
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmployeeService {
 
-    List<Employee> getAllEmployees();
+    private final EmployeeRepository employeeRepository;
 
-    void saveEmployee(Employee employee);
+    public List<Employee> findAllEmployees() {
+        log.trace("Call findAllEmployees");
+        return employeeRepository.findAll();
+    }
 
-    Employee getEmployee(int id);
+    @Transactional
+    public Employee createEmployee(String name, String surname, String department, int salary) {
+        log.trace("Call createEmployee : name={}, surname={}, department={}, salary={}", name, surname, department, salary);
+        return employeeRepository.save(new Employee()
+                .setName(name)
+                .setSurname(surname)
+                .setDepartment(department)
+                .setSalary(salary));
+    }
 
-    void deleteEmployee(int id);
+    @Transactional
+    public Employee updateEmployee(int id, EmployeeDTO employeeDTO) {
+        log.trace("Call updateEmployee : id={}, employeeDTO={}", id, employeeDTO);
+        Employee employee = findEmployeeById(id);
+        return employee.setName(employeeDTO.getName())
+                .setSurname(employeeDTO.getSurname())
+                .setDepartment(employeeDTO.getDepartment())
+                .setSalary(employeeDTO.getSalary());
+    }
 
-    List<Employee> findAllByName(String name);
+    public Employee findEmployeeById(int id) {
+        log.trace("Call findEmployeeById : id={}", id);
+        Optional<Employee> optional = employeeRepository.findById(id);
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        throw new EmployeeNotFoundException(id);
+    }
+
+    @Transactional
+    public void deleteEmployeeById(int id) {
+        log.trace("Call deleteEmployeeById : id={}", id);
+        Employee employee = findEmployeeById(id);
+        employeeRepository.delete(employee);
+    }
+
+    public List<Employee> findAllEmployeesByName(String name) {
+        log.trace("Call findAllEmployeesByName : name={}", name);
+        return employeeRepository.findByNameIsContaining(name);
+    }
 }
