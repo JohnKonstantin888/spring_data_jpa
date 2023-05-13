74a57ca3ed6da54654f6a5dc28cdefbe6adb7a90
Konstantin Kovalyov <Konstantin-kovalyo@yandex.ru>
2023-05-13 14:03:07 +0300

    correct controller

--- src/main/java/com/konstantin/spring/springboot/spring_data_jpa/controller/MyRESTController.java
+++ src/main/java/com/konstantin/spring/springboot/spring_data_jpa/controller/MyRESTController.java
@@ -1,54 +1,65 @@
 package com.konstantin.spring.springboot.spring_data_jpa.controller;
 
+import com.konstantin.spring.springboot.spring_data_jpa.dto.EmployeeDTO;
 import com.konstantin.spring.springboot.spring_data_jpa.entity.Employee;
+import com.konstantin.spring.springboot.spring_data_jpa.exceptions.NotValidParameterException;
 import com.konstantin.spring.springboot.spring_data_jpa.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+@Slf4j
 public class MyRESTController {
 
-    @Autowired
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     @GetMapping("/employees")
-    public List<Employee> showAllEmployees() {
-        List<Employee> allEmployees = employeeService.getAllEmployees();
-        return allEmployees;
+    public List<Employee> findAllEmployees() {
+        log.trace("Call findAllEmployees");
+        return employeeService.findAllEmployees();
     }
 
-    @GetMapping("/employees/{id}")
+    @GetMapping("/employee/{id}")
     public Employee getEmployee(@PathVariable int id) {
-        Employee employee = employeeService.getEmployee(id);
-
-        return employee;
+        log.trace("Call getEmployee : id={}", id);
+        if (id <= 0) {
+            throw new NotValidParameterException(id);
+        }
+        return employeeService.findEmployeeById(id);
     }
 
-    @PostMapping("/employees")
-    public Employee addNewEmployee(@RequestBody Employee employee) {
-        employeeService.saveEmployee(employee);
-        return employee;
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestParam String name,
+                                   @RequestParam String surname,
+                                   @RequestParam String department,
+                                   @RequestParam int salary) {
+        log.trace("Call createEmployee : name={}, surname={}, department={}, salary={}", name, surname, department, salary);
+        return employeeService.createEmployee(name, surname, department, salary);
     }
 
-    @PutMapping("/employees")
-    public Employee updateEmployee(@RequestBody Employee employee) {
-        employeeService.saveEmployee(employee);
-        return employee;
+    @PutMapping("/employee/{id}")
+    public Employee updateEmployee(@PathVariable int id,
+                                   @RequestBody EmployeeDTO employeeDTO) {
+        log.trace("Call updateEmployee : id={}, employee={}", id, employeeDTO);
+        return employeeService.updateEmployee(id, employeeDTO);
     }
 
     @DeleteMapping("/employees/{id}")
     public String deleteEmployee(@PathVariable int id) {
-        Employee employee = employeeService.getEmployee(id);
-
-        employeeService.deleteEmployee(id);
+        log.trace("Call deleteEmployee : id={}", id);
+        employeeService.deleteEmployeeById(id);
         return "Employee with ID = " + id + " was deleted.";
     }
 
-    @GetMapping("/employees/name/{name}")
-    public List<Employee> showAllEmployeesByName(@PathVariable String name) {
-        return employeeService.findAllByName(name);
+    @GetMapping("/employee/{name}")
+    public List<Employee> findAllEmployeesByName(@PathVariable String name) {
+        log.trace("Call findAllEmployeesByName : name={}", name);
+        return employeeService.findAllEmployeesByName(name);
     }
 }
